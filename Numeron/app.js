26e4630da6ddb148edeb355a319340ca030801c4
Shreyas-Kalvium <160594041+Shreyas-Kalvium@users.noreply.github.com>
2024-03-20 14:45:46 +0530

    Numeron - Game

---
+++ Numeron/app.js
@@ -0,0 +1,8 @@
+// Iteration 1: Make the Play button functional.
+// Description: When the Play button is clicked the game page should be displayed.
+const playButton = document.getElementById("play-button")
+playButton.onclick = loadpage
+
+function loadpage(){
+    window.location.assign("game.html")
+}
