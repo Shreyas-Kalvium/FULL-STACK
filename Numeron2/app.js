9e431a5d669580af955c2326c3c0b64d6040f4fa
Shreyas-Kalvium <160594041+Shreyas-Kalvium@users.noreply.github.com>
2024-03-21 18:21:36 +0530

    Numeron2

---
+++ Numeron2/app.js
@@ -0,0 +1,6 @@
+// Iteration 1: Making the play button in the index.html functional.
+// Description: When the play button is clicked the game.html page should be opened
+const playButton=document.getElementById("play-button")
+playButton.onclick=()=>{
+    location.href="./game.html"
+}
